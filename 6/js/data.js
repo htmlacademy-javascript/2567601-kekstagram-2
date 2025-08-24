b8464b5f7c7ecbd0fd7467770a35419bc118b192
Keks <keks@htmlacademy.ru>
2025-08-24 19:05:52 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/data.js
@@ -0,0 +1,58 @@
+import { getRandomArrayElement, getRandomInteger } from './util';
+
+const PHOTO_DESCRIPTION = [
+  'братите внимание, в тексте задания вы будете',
+  'встречать текст',
+  'в фигурных скобках. Такой текст будет означать, что на месте этого',
+  'текста должно появиться значение, которое вы',
+  'возьмёте из данных. Например, в шаблоне может быть написано <div>{{x}}</div>, и это',
+  'будет значить, что {{x}} нужно заменить на значение переменной x. Если',
+  'переменная будет равна 100, то разметка',
+];
+
+const MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const NAMES = [
+  'Рузиль',
+  'Осман',
+  'Влад',
+  'Альберт',
+  'Максим',
+  'Данил',
+  'Аделя'
+];
+
+const getRandomId = () => {
+  let id = 0;
+  return function () {
+    id += 1;
+    return id;
+  };
+};
+
+const photoId = getRandomId();
+const commentId = getRandomId();
+
+const createComments = () => ({
+  id: commentId(),
+  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+  message: getRandomArrayElement(MESSAGE),
+  name: getRandomArrayElement(NAMES),
+});
+
+const createPhoto = () => ({
+  id: photoId(),
+  url: `photos/${photoId()}.jpg`,
+  description: getRandomArrayElement(PHOTO_DESCRIPTION),
+  likes: getRandomInteger(15, 200),
+  comments: Array.from({ length: getRandomInteger(0, 30) }, createComments),
+});
+
+export const createPhotos = () => Array.from({ length: 25 }, createPhoto);
---
+++ 6/js/main.js
@@ -0,0 +1,11 @@
+import './data';
+import { picturesNode } from './photos';
+import { openFullPhoto } from './renderFullPhoto';
+
+picturesNode.addEventListener('click', (evt) => {
+  const pictureElement = evt.target.closest('.picture');
+
+  if (pictureElement) {
+    openFullPhoto(pictureElement.dataset.pictureId);
+  }
+});
