b0f2f5cdc24edfe92d77e7ddbf5716c4a49a4cd5
Keks <keks@htmlacademy.ru>
2025-09-06 17:53:47 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/api.js
@@ -0,0 +1,14 @@
+export const photoData = {
+  get: async () => {
+    const response = await fetch('https://31.javascript.htmlacademy.pro/kekstagram/data');
+    return await response.json();
+  },
+  send: async (newData) => {
+
+    const response = await fetch(' https://31.javascript.htmlacademy.pro/kekstagram', {
+      method: 'POST',
+      body: newData,
+    });
+    return await response.json();
+  },
+};
